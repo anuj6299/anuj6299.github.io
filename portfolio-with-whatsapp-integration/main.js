842ed2ac188ee29bfa7a1e6eca024ff0fb7ff0e8
Anuj Sharma <43301340+anuj6299@users.noreply.github.com>
2020-07-08 01:23:20 +0530

    Add files via upload

---
+++ portfolio-with-whatsapp-integration/main.js
@@ -0,0 +1,42 @@
+// Function expression to select elements
+const selectElement = (s) => document.querySelector(s);
+const navLinks = document.querySelectorAll(".nav-link");
+
+selectElement(".burger-menu-icon").addEventListener("click", () => {
+  // Toggle the nav on menu icon click
+  selectElement(".nav-list").classList.toggle("active");
+
+  // Animate Links
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.5
+      }s`;
+    }
+  });
+
+  //Burger Animation
+  selectElement(".burger-menu-icon").classList.toggle("toggle");
+});
+
+//close navbar on nav link click
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    selectElement(".nav-list").classList.toggle("active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+
+    selectElement(".burger-menu-icon").classList.toggle("toggle");
+  });
+});
